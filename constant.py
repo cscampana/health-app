ea55a9370f0cfc11ac1930c352bb926f9aee21df
Caike Campana <csallesc@ucsd.edu>
2021-09-01 02:01:14 -0700

    Added menu logic and reformatted the variable checking.

--- constant.py
+++ constant.py
@@ -8,7 +8,19 @@ IN_TO_CM = 2.54
 WELCOME_MSG = "Hey welcome to the Health App, please select the appropriate option:"
 OPTIONS = "Choose the appropriate option:\r\n" \
           "1) Enter the macros of some food to get the estimated calories \r\n" \
-          "2) Enter your height and weight to calculate your BMI"
+          "2) Enter your height and weight to calculate your BMI\r\n" \
+          "Option: "
 OPTION_MACROS = "Enter the following information about the food you want to calculate:"
-CARB_AMOUNT = "Quantity of carbs in grams: "
+CARB_AMOUNT = "Amount of carbs in grams: "
 PROTEIN_AMOUNT = "Amount of proteins in grams: "
+FAT_AMOUNT = "Amount of fat in grams: "
+RESULT_MACRO = "The total calories in your food is "
+
+IMPERIAL_OR_METRIC = "Enter I for imperial system (lbs and inches) or M for metric system (kgs and cm): "
+IMPERIAL_HEIGHT = "Enter your height in inches: "
+IMPERIAL_WEIGHT = "Enter your weight in pounds: "
+METRIC_HEIGHT = "Enter your height in centimeters: "
+METRIC_WEIGHT = "Enter your weight in kilograms: "
+
+CORRECT_INPUT_SYSTEM = "Please select one of the valid options (M or I): "
+RESULT_BMI = "Your current BMI is: "
